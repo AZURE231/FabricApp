c7d0c87df2fdf3a032ab3e451dfda37c86bf1e44
anhturhyx <anhtunguyenhuynh1910@gmail.com>
2022-12-02 11:28:33 +0700

    update 2b 2c

--- sql/task2b.sql
+++ sql/task2b.sql
@@ -1,5 +1,4 @@
-select tablex.order_id from(
-select bolt.order_id, category_supplier.supplier_id
-from bolt
-join category_supplier on bolt.category_id = category_supplier.category_id ) as tablex
-join supplier on tablex.supplier_id = supplier.id where supplier.sname = 'ToLaSupplierA';
+select tablex.order_id 
+from( select bolt.order_id, category.supplier_id 
+from bolt join category on bolt.category_id = category.id ) as tablex 
+join supplier on tablex.supplier_id = supplier.id where supplier.name = 'ToLaSupplierA'
--- sql/task2c.sql
+++ sql/task2c.sql
@@ -1,11 +1,18 @@
-delimiter //
-create procedure paylistpersup (supid CHAR(10))
-begin
-select ttable.category_id, ttable.totalpayment from (
-select category_supplier.supplier_id, supplement.category_id, sum(supplement.price) as totalpayment
-from category_supplier
-join supplement on category_supplier.category_id = supplement.category_id group by supplement.category_id
-)as ttable where supplier_id = supid;
+   delimiter //
+    CREATE FUNCTION `totalpayment`(cate enum('1','2','3','4','5','6')) RETURNS bigint
+        READS SQL DATA
+        DETERMINISTIC
+BEGIN
+    DECLARE v_emp_count bigint;
+    set v_emp_count = (select sum(supplement.purchase_price) 
+	as totalpayment from  supplement 
+    where category_id = cate);
+    RETURN v_emp_count;
+END  //
 
-end //
-call paylistpersup('sup0000001')
+CREATE PROCEDURE `paylistpersupp`(supid CHAR(10))
+begin select distinct ttable.category_id, fabric.totalpayment(ttable.category_id)
+ from (select category.supplier_id, supplement.category_id
+ from category join supplement on category.id = supplement.category_id) as ttable
+where supplier_id = supid;  
+ end
